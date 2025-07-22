e8d06ad80da18ab5e463baa65efe4e17120181fb
shubh-codes7 <shubhamcodes7@gmail.com>
2025-07-22 15:58:33 +0530

    bug fix

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
-import Signup from '../pages/Signup'
+import SignUp from '../pages/SignUp'
 
 const routes = (
     <Router>
       <Routes>
         <Route path='/' element = {<Login/>} />
-        <Route path='/signup' element = {<Signup/>} />
+        <Route path='/signup' element = {<SignUp/>} />
         <Route path='/home' element = {<Home/>} />
       </Routes>
     </Router>
