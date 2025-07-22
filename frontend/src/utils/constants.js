23bbdccb87f621792e28e26cf9702de40fe78aae
shubh-codes7 <shubhamcodes7@gmail.com>
2025-07-22 15:55:08 +0530

    vercel deploy

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8000/"
+// export const BASE_URL = "http://localhost:8000/" for devlopement
+export const BASE_URL = "https://notes-app-pern-stack.onrender.com/" //for productrion
